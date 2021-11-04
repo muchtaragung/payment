551c9c8c0f387fb9c6a64f36e99637b68838a23b
muchtaragung <muchtaragung7@gmail.com>
2021-11-04 12:23:04 +0700

    add favicon

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -6,8 +6,10 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="Created by : Alex Jhosan https://github.com/ajhosan">
+    <link rel="apple-touch-icon" sizes="180x180" href="<?= base_url() ?>asset/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?= base_url() ?>asset/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?= base_url() ?>asset/favicon/favicon-16x16.png">
+    <link rel="manifest" href="<?= base_url() ?>asset/favicon/site.webmanifest">
 
     <title><?= $title; ?></title>
 
