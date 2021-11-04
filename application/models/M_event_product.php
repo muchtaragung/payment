96aacb65d00c2cd6a2a1c61b304feb21f4bf9838
muchtaragung <muchtaragung7@gmail.com>
2021-11-04 12:13:20 +0700

    fix event payment

--- application/models/M_event_product.php
+++ application/models/M_event_product.php
@@ -9,7 +9,6 @@ class M_event_product extends CI_Model
         $this->db->select("*");
         $this->db->from('event_product');
         $this->db->join('user', 'user.id_user = event_product.id_user');
-        $this->db->join('action_button', 'action_button.id_actionbutton = event_product.id_actionbutton', 'inner');
         $this->db->order_by('id_events', 'DESC');
         return $this->db->get();
     }
@@ -19,7 +18,6 @@ class M_event_product extends CI_Model
         $this->db->select("*");
         $this->db->from('event_product');
         $this->db->join('user', 'user.id_user = event_product.id_user');
-        $this->db->join('action_button', 'action_button.id_actionbutton = event_product.id_actionbutton', 'inner');
         $this->db->where('id_events', $id_event);
         return $this->db->get();
     }
@@ -29,8 +27,21 @@ class M_event_product extends CI_Model
         $this->db->select("*");
         $this->db->from('event_product');
         $this->db->join('user', 'user.id_user = event_product.id_user');
-        $this->db->join('action_button', 'action_button.id_actionbutton = event_product.id_actionbutton', 'inner');
         $this->db->where('event_product.slug_event', $slug);
         return $this->db->get();
+    }
+    public function add_event($data)
+    {
+        return $this->db->insert('event_product', $data);
+    }
+    public function update_event($id, $data)
+    {
+        $this->db->where('id_events', $id);
+        $this->db->update('event_product', $data);
+    }
+    public function delete_event($id)
+    {
+        $this->db->where('id_events', $id);
+        $this->db->delete('event_product');
     }
 }
