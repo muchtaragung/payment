3d8ee656ef7121f5c07697b4a858c6b0de43b419
Rahmat Gibran Zulbadriansyah <rgibran234@gmail.com>
2021-11-09 14:47:21 +0700

    crd event

---
+++ application/controllers/admin/Event.php
@@ -0,0 +1,103 @@
+<?php
+
+class Event extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('Event_model', 'event');
+        $this->load->model('User_model', 'user');
+    }
+
+    public function list()
+    {
+        $join = [
+            ['user', 'user.id_user = event.id_user']
+        ];
+
+        $data['title']  = "List Event";
+        $data['events'] = $this->event->get_join($join)->result();
+        $this->load->view('admin/event/list', $data);
+    }
+
+    public function add()
+    {
+        $data['title'] = "Add Event";
+        $data['user'] = $this->user->get_all()->result();
+        $this->load->view('admin/event/add', $data);
+    }
+
+    public function save()
+    {
+        $event['nama_event']  = $this->input->post('nama_event');
+        $event['slug_event']  = str_replace(' ', '-', strtolower($event['nama_event']));
+        // $event['image_event'] = $this->_upload();
+        $event['description'] = $this->input->post('description');
+        $event['quantity']    = $this->input->post('quantity');
+        $event['link_event']  = $this->input->post('link_event');
+        $event['date_event']  = $this->input->post('date_event');
+        $event['start_time']  = $this->input->post('start_time');
+        $event['end_time']    = $this->input->post('end_time');
+        $event['price']       = $this->input->post('price');
+        $event['id_user']     = $this->input->post('id_user');
+
+        $data = $this->event->save($event);
+
+        $this->session->set_flashdata('msg', 'Berhasil Membuat Data Sales');
+        return redirect('admin/event/list');
+    }
+
+    public function edit($id_event)
+    {
+        $data['event'] = $this->event->get_where(['id_event' => $id_event])->row();
+
+        $this->load->view('admin/event/edit', $data);
+    }
+
+    public function update()
+    {
+        $event['id_event'] = $this->input->post('id_event');
+        $event['nama_event']  = $this->input->post('nama_event');
+        $event['slug_event']  = $this->input->post('email');
+        // $event['image_event'] = $this->_upload();
+        $event['description'] = $this->input->post('description');
+        $event['quantity']    = $this->input->post('quantity');
+        $event['link_event']  = $this->input->post('link_event');
+        $event['link_credit'] = $this->input->post('link_credit');
+        $event['date_event']  = $this->input->post('date_event');
+        $event['start_time']  = $this->input->post('time_event');
+        $event['end_time']    = $this->input->post('time_event');
+        $event['price']       = $this->input->post('price');
+        $event['id_user']     = $this->input->post('id_user');
+
+        $data = $this->event->update($event);
+
+        $this->session->set_flashdata('msg', 'Berhasil Mengupdate Data Sales');
+
+        return redirect('admin/event/list');
+    }
+
+    public function delete($id_event)
+    {
+        $this->event->delete(['id_event' => $id_event]);
+        $this->session->set_flashdata('msg', 'Berhasil Menghapus Data Sales');
+        return redirect('admin/event/list');
+    }
+
+    private function _upload()
+    {
+        $config['upload_path']   = './assets/events/images/';
+        $config['allowed_types'] = 'gif|jpg|png';
+        $config['file_name']     = uniqid();
+        $config['overwrite']     = true;
+        $config['encrypt_name']  = false;
+
+        $this->load->library('upload', $config);
+
+        if (!$this->upload->do_upload('image')) {
+            print_r($this->upload->display_errors());
+        } else {
+            return $this->upload->data("file_name");
+        }
+    }
+}
---
+++ application/views/admin/event/list.php
@@ -0,0 +1,163 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php $this->load->view('layouts/head'); ?>
+</head>
+
+<body id="page-top">
+
+    <!-- Page Wrapper -->
+    <div id="wrapper">
+
+        <!-- Sidebar -->
+        <?php $this->load->view('layouts/sidebar'); ?>
+        <!-- End of Sidebar -->
+
+        <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column">
+
+            <!-- Main Content -->
+            <div id="content">
+
+                <!-- Topbar -->
+                <?php $this->load->view('layouts/topbar.php'); ?>
+                <!-- End of Topbar -->
+
+                <!-- Begin Page Content -->
+                <div class="container-fluid">
+
+                    <!-- Page Heading -->
+                    <h1 class="h3 mb-4 text-gray-800">List event</h1>
+
+                    <?php if ($this->session->flashdata('msg')) : ?>
+                        <div class="alert alert-success" role="alert">
+                            <?= $this->session->flashdata('msg') ?>
+                        </div>
+                    <?php elseif ($this->session->flashdata('error')) : ?>
+                        <div class="alert alert-danger" role="alert">
+                            <?= $this->session->flashdata('error') ?>
+                        </div>
+                    <?php endif ?>
+
+                    <!-- DataTales Example -->
+                    <div class="card shadow mb-4">
+                        <div class="card-header py-3">
+                            <h6 class="m-0 font-weight-bold text-primary">Table event</h6>
+                            <a href="<?= site_url('admin/event/add') ?>" class="btn btn-primary float-right">Tambah event</a>
+                        </div>
+                        <div class="card-body">
+                            <div class="table-responsive">
+                                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                                    <thead>
+                                        <tr>
+                                            <th>No</th>
+                                            <th>Nama Event</th>
+                                            <th>Harga</th>
+                                            <th>Tanggal</th>
+                                            <th>Nama event</th>
+                                            <th>Action</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <?php
+                                        $no = 1;
+                                        foreach ($events as $data) : ?>
+                                            <tr>
+                                                <td><?= $no++ ?></td>
+                                                <td><b><?= $data->nama_event ?></b></td>
+                                                <td>Rp.<?= number_format($data->price, 0, ",", "."); ?></td>
+                                                <td><?= date('d-m-Y', strtotime($data->date_event)); ?></td>
+                                                <td><?= $data->name ?></td>
+                                                <td>
+                                                    <a class="btn btn-md btn-info" href="<?= base_url(); ?>admin/event/edit/<?= $data->id_event; ?>">Edit</a>
+                                                    <a class="btn btn-md btn-danger" onclick="return confirm('Apakah anda yakin ingin menghapus data ini?')" href="<?= base_url(); ?>admin/event/delete/<?= $data->id_event; ?>">Hapus</a>
+                                                </td>
+                                            </tr>
+                                        <?php endforeach ?>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                <!-- /.container-fluid -->
+
+            </div>
+            <!-- End of Main Content -->
+
+            <!-- Footer -->
+            <?php $this->load->view('layouts/footer'); ?>
+            <!-- End of Footer -->
+
+        </div>
+        <!-- End of Content Wrapper -->
+
+    </div>
+    <!-- End of Page Wrapper -->
+
+    <!-- Scroll to Top Button-->
+    <a class="scroll-to-top rounded" href="#page-top">
+        <i class="fas fa-angle-up"></i>
+    </a>
+
+    <!-- Modal -->
+    <div class="modal fade" id="delete-modal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Delete event</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <form class="form-horizontal">
+                    <div class="modal-body">
+
+                        <input type="hidden" name="id_event" id="id_event" value="">
+                        <div class="alert alert-warning">
+                            <p>Apakah Anda yakin mau menghapus data event ini?</p>
+                        </div>
+
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Tutup</button>
+                        <button type="button" class="btn btn-danger" id="button-delete">Hapus</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <?php $this->load->view('layouts/script'); ?>
+    <script>
+        $(document).ready(function() {
+
+            //delete modal
+            $('.delete-button').on('click', function() {
+                var id_event = $(this).attr('data');
+                $('#delete-modal').modal('show');
+                $('[name="id_event"]').val(id_event);
+            });
+
+            //Hapus 
+            $('#button-delete').on('click', function() {
+                var id_event = $('#id_event').val();
+                $.ajax({
+                    type: "POST",
+                    url: "<?php echo site_url('admin/event/delete') ?>",
+                    dataType: "JSON",
+                    data: {
+                        id_event: id_event
+                    },
+                    success: function(data) {
+                        $('#delete-modal').modal('hide');
+                        location.reload();
+                    }
+                });
+                return false;
+            });
+        });
+    </script>
+</body>
